ca74520823870c767ac70cc236c28af1ae566dbc
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2020-11-24 09:15:38 +0200

    Lahendused

---
+++ 1.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Ülesanne 1.
+ * Anna-Liisa Saks; 24.11.2020
+ */
+
+$nimi = "Anna-Liisa Saks";
+$sunniaasta = "13.05.1998";
+$tahtkuju = "sõnn";
+
+
+    echo "Minu nimi on " .$nimi. "\n";
+    echo "Minu sünniaasta on " .$sunniaasta. "\n";
+    echo "Minu tähtkuju on " .$tahtkuju. "\n";
+echo "\"It's My Life\" - Bon Jovi". "\n";
+echo "(\ (\  \n";
+echo "(-.-) \n";
+echo "o_(\")(\")";
